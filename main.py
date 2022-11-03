5f9a7c438551ac04802b548cbea9a267cb843061
BonDevil <82535836+BonDevil@users.noreply.github.com>
2022-11-03 16:31:42 +0100

    Added insert methods

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-# Author: Jakub Szwedowicz
-# Date: 02.11.2022
-# e-mail: kuba.szwedowicz@gmail.com
-
-import psycopg2
 from configparser import ConfigParser
+import psycopg2
 from database_tables import Tables
-from complex_structures import Locations
 
 
-def config(filename: str = 'Config/database.ini', section: str = 'postgresql') -> dict:
+def config(filename=r'C:\Users\Piotr\PycharmProjects\Database_project\Config\database.ini', section='postgresql'):
+    # create a parser
     parser = ConfigParser()
+    # read config file
     parser.read(filename)
 
+    # get section, default to postgresql
     db = {}
     if parser.has_section(section):
         params = parser.items(section)
@@ -24,64 +22,167 @@ def config(filename: str = 'Config/database.ini', section: str = 'postgresql') -
 
 
 def connect():
-    params = config()
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
 
-    print('Connecting to the PostgreSQL database...')
-    with psycopg2.connect(**params) as conn:
-        with conn.cursor() as cur:
-            print('PostgreSQL database version:')
-            cur.execute('SELECT version()')
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
 
-            db_version = cur.fetchone()
-            print(db_version)
+        # create a cursor
+        cur = conn.cursor()
 
-            cur.close()
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
         if conn is not None:
             conn.close()
             print('Database connection closed.')
 
 
-def add_partv2(part_name, vendor_list):
-    insert_part = 'INSERT INTO parts(part_name) VALUES(%s) RETURNING part_id;'
-    assign_vendor = 'INSERT INTO vendor_parts(vendor_id, part_id) VALUES(%s,%s)'
-
-    params = config()
-    with psycopg2.connect(**params) as conn:
-        with conn.cursor() as cur:
-            cur.execute(insert_part, (part_name,))
-            part_id = cur.fetchone()[0]
-
-            for vendor_id in vendor_list:
-                cur.execute(assign_vendor, (vendor_id, part_id))
-
-            conn.commit()
+def insert_building():
+    values = Tables.generateBuilding()
+    sql = f""" INSERT INTO building (name, street, buildingnumber, city, postalcode) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
 
 
-def execute_add_part():
-    add_partv2('SIM Tray', (1, 2))
-    add_partv2('Speaker', (3, 4))
-    add_partv2('Vibrator', (5, 6))
-    add_partv2('Antenna', (6, 7))
-    add_partv2('Home Button', (1, 5))
-    add_partv2('LTE Modem', (1, 5))
+def insert_student():
+    values = Tables.generateStudent()
+    sql = f""" INSERT INTO student (name, lastname, studentnumber, buildingnumber, apartmentnumber, street, 
+                city, postalcode, email, phone) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
 
 
-def examples() -> None:
-    # Tables.tables_statuses_example()
+def insert_employee():
+    values = Tables.generateEmployee()
+    sql = f""" INSERT INTO employee (name, lastname, street, apartmentnumber, buildingnumber, city, postalcode, email,
+            phone, salary) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
 
-    # Locations.locations_example()
 
-    print(Tables.StudentStatus.generate_status(1))
-    print(Tables.ApplicationStatus.generate_status(2))
-    print(Tables.ApplicationType.generate_status())
-    print(Tables.CardStatus.generate_status())
+def insert_application():
+    values = Tables.generateApplication()
+    sql = f""" INSERT INTO application (id_student, id_employee, status, receivedate, id_applicationtype,
+                id_utensils) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
 
-def main():
-    Tables.init_statuses()
-    Locations.init_locations()
 
-    examples()
+def insert_payment():
+    values = Tables.generatePayment()
+    sql = f""" INSERT INTO payment (id_student, amount, paymentdate) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_charge():
+    values = Tables.generateCharge()
+    sql = f""" INSERT INTO charge (id_student, chargedate, amount) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_rent():
+    values = Tables.generateStudent()
+    sql = f""" INSERT INTO rent (id_room, id_student, id_application, expiredate) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_floor():
+    values = Tables.generateFloor()
+    sql = f""" INSERT INTO floor (number, id_building) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_kitchen():
+    values = Tables.generateKitchen()
+    sql = f""" INSERT INTO kitchen (id_floor) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_building_employee():
+    values = Tables.generateBuildingEmployee()
+    sql = f""" INSERT INTO building_employee (id_floor) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_laundry():
+    values = Tables.generateLaundry()
+    sql = f""" INSERT INTO laundry (id_floor) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_parkingspot():
+    values = Tables.generateParkingSpot()
+    sql = f""" INSERT INTO parkingspot (number, id_building) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_room():
+    values = Tables.generateRoom()
+    sql = f""" INSERT INTO room (number, id_module) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert_module():
+    values = Tables.generateModule()
+    sql = f""" INSERT INTO room (id_floor) VALUES {values} returning id"""
+    id = insert(sql, values)
+    return id
+
+
+def insert(sql, values):
+    conn = None
+    id = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        id = cur.fetchone()[0]
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return id
+
+
+
+def populate_buildings(quantity):
+    buildings = []
+    for i in range(quantity):
+        buildings.append(insert_building())
+
+    return buildings
+
 
 
 if __name__ == '__main__':
-    main()
+    buildings = populate_buildings(5)
+
