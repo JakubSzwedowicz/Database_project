d06de8cbbcaac13a755665de2bb22c24d5757a06
filipmaciejak <fimaciejak@gmail.com>
2022-11-05 00:04:20 +0100

    take lists in gen.py

--- Src/gen.py
+++ Src/gen.py
@@ -27,39 +27,58 @@ def gen_Employee():
     return f"('{fake.first_name()}','{fake.last_name()}','{street.name}','{fake.building_number()}','{fake.building_number()}','{city_name}','{street.postal_code}','{fake.ascii_email()}','{fake.phone_number()}','{random.randrange(1000000) / 100.0}')"
 
 
-def gen_Application(ID_Student, ID_Employee, ID_ApplicationStatus, ID_Utensils, ID_ApplicationType):
+def gen_Application(students, employees, applicationstatuses, utensils, applicationtypes):
+    ID_Student = random.choice(students)
+    ID_Employee = random.choice(employees)
+    ID_ApplicationStatus = random.choice(applicationstatuses)
+    ID_Utensils = random.choice(utensils)
+    ID_ApplicationType = random.choice(applicationtypes)
     return f"('{ID_Student}','{ID_Employee}','{ID_ApplicationStatus}','{fake.date()}','{ID_Utensils}','{ID_ApplicationType}')"
 
 
-def gen_Utensils(ID_Laundry, ID_Kitchen, ID_Room):
+def gen_Utensils(laundries, kitchens, rooms):
+    ID_Laundry = random.choice(laundries)
+    ID_Kitchen = random.choice(kitchens)
+    ID_Room = random.choice(rooms)
     return f"('{fake.paragraph(nb_sentences=1)}','{random.randint(1, 8)}','{ID_Laundry}','{ID_Kitchen}','{ID_Room}')"
 
 
-def gen_Floor(ID_Building):
+def gen_Floor(buildings):
+    ID_Building = random.choice(buildings)
     return f"('{random.randint(0, 10)}','{ID_Building}')"
 
 
-def gen_Room(ID_Module):
+def gen_Room(modules):
+    ID_Module = modules
     return f"('{random.randint(0, 999)}','{ID_Module}')"
 
 
-def gen_Rent(ID_Room, ID_Student, ID_Application):
+def gen_Rent(rooms, students, applications):
+    ID_Room = random.choice(rooms)
+    ID_Student = random.choice(students)
+    ID_Application = random.choice(applications)
     return f"('{ID_Room}','{ID_Student}','{ID_Application}','{fake.date()}')"
 
 
-def gen_Charge(ID_Student):
+def gen_Charge(students):
+    ID_Student = random.choice(students)
     return f"('{ID_Student}','{fake.date()}','{random.randrange(100000) / 100.0}')"
 
 
-def gen_Payment(ID_Student):
+def gen_Payment(students):
+    ID_Student = random.choice(students)
     return f"('{ID_Student}','{random.randrange(100000) / 100.0}','{fake.date()}')"
 
 
-def gen_ParkingSpot(ID_Building):
+def gen_ParkingSpot(buildings):
+    ID_Building = random.choice(buildings)
     return f"('{random.randint(1, 200)}','{ID_Building}')"
 
 
-def gen_ResidentCard(ID_ParkingSpot, ID_Student, ID_CardStatus):
+def gen_ResidentCard(parkingspots, students, cardstatuses):
+    ID_ParkingSpot = random.choice(parkingspots)
+    ID_Student = random.choice(students)
+    ID_CardStatus = random.choice(cardstatuses)
     return f"('{ID_ParkingSpot}','{ID_Student}','{fake.date()}','{ID_CardStatus}')"
 
 
