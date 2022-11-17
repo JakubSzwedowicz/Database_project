859757f6a23ff45060904bc639ee5459d9f30af5
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2022-11-17 03:49:50 +0100

    Add script with created indices

--- Scripts/apply_indexing.sql
+++ Scripts/apply_indexing.sql
@@ -3,7 +3,9 @@
 
 
 /* studenci, ktorzy maja co najmniej jeden nowo zlozony wniosek  */
-
+CREATE INDEX student_lastName_Name_index ON Student (lastname, name);
+/* Dlaczego sortowanie nie działa? */
+/* DROP INDEX student_lastName_Name_index; */
 
 
 /* studenci, ktorym wygasla karta mieszkanca (nie posiadają aktualnie aktywnej karty)*/
@@ -13,11 +15,13 @@
 
 
 /* Budynki */
-
+CREATE INDEX floor_in_building_index ON floor (id_building);
+CREATE INDEX module_on_floor_index ON module (id_floor);
+CREATE INDEX room_in_module_index ON room (id_module);
 
 
 /* Liczba aktywnych i i niekatywnych studentow */
-
+CREATE INDEX studentstatus_of_student_index ON Student (id_studentstatus);
 
 
 /* Wyswietlan wszystkich wynajmow*/
@@ -26,11 +30,11 @@
 
 /* Wszystkie sumy platnosci i obciazenia studentow - strasznie dlugo */
 CREATE INDEX payment_of_student_index ON payment (id_student);
-CREATE INDEX payment_of_student_index ON payment (id_student);
-
+CREATE INDEX charge_of_student_index ON charge (id_student);
+/*
+AD 1. hash index dodaje za duży overhead i zapytanie jest wtedy powolne.
+*/
 
 /* Pracownicy i liczba wnioskow do nich przypisana */
-
-
 
 /* Dane budynkow i ile jest w nich pracownikow */
