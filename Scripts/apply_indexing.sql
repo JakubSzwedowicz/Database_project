c960e6ef147d9ea9097340c20a0f44dfe2de4352
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2022-11-17 01:17:29 +0100

    Add script responsible for creating indices

---
+++ Scripts/apply_indexing.sql
@@ -0,0 +1,36 @@
+/* wyswietlanie zlozonych wnioskow */
+
+
+
+/* studenci, ktorzy maja co najmniej jeden nowo zlozony wniosek  */
+
+
+
+/* studenci, ktorym wygasla karta mieszkanca (nie posiadają aktualnie aktywnej karty)*/
+
+
+/* Obciazenia studentow */
+
+
+/* Budynki */
+
+
+
+/* Liczba aktywnych i i niekatywnych studentow */
+
+
+
+/* Wyswietlan wszystkich wynajmow*/
+
+
+
+/* Wszystkie sumy platnosci i obciazenia studentow - strasznie dlugo */
+CREATE INDEX payment_of_student_index ON payment (id_student);
+CREATE INDEX payment_of_student_index ON payment (id_student);
+
+
+/* Pracownicy i liczba wnioskow do nich przypisana */
+
+
+
+/* Dane budynkow i ile jest w nich pracownikow */
